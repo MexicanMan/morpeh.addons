fc4ee7ba87d85616381d4dc4839e92662015888f
Mexican_Man <sombrero13nn@gmail.com>
2023-08-18 01:15:27 +0300

    Added Base Feature Code Installer

---
+++ Assets/Scripts/Sample/FeaturesInstaller.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.Sample.PlayerInput;
+using Scellecs.Morpeh.Addons.Feature;
+using Scellecs.Morpeh.Addons.Feature.Unity;
+
+namespace Assets.Scripts.Sample
+{
+    public class FeaturesInstaller : CodeFeaturesInstaller
+    {
+        protected override void InitializeShared()
+        {
+        }
+
+        protected override UpdateFeature[] InitializeUpdateFeatures()
+        {
+            return new UpdateFeature[] 
+            { 
+                new PlayerInputFeature() 
+            };
+        }
+
+        protected override FixedUpdateFeature[] InitializeFixedUpdateFeatures()
+        {
+            return new FixedUpdateFeature[] { };
+        }
+
+        protected override LateUpdateFeature[] InitializeLateUpdateFeatures()
+        {
+            return new LateUpdateFeature[] { };
+        }
+    }
+}
