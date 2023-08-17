521938f3ccd8b86d6b110f0e876811d2b98a1b9e
Mexican_Man <sombrero13nn@gmail.com>
2023-08-18 01:15:27 +0300

    Added Base Feature Code Installer

---
+++ Assets/Scripts/morpeh.addons/Feature/Unity/CodeFeaturesInstaller.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace Scellecs.Morpeh.Addons.Feature.Unity
+{
+    public abstract class CodeFeaturesInstaller : BaseInstaller
+    {
+        protected World defaultWorld;
+
+        private UpdateFeature[] _updateFeatures;
+        private FixedUpdateFeature[] _fixedUpdateFeatures;
+        private LateUpdateFeature[] _lateUpdateFeatures;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void AddPlugins()
+        {
+            MorpehAddons.Initialize();
+        }
+
+        protected void Awake()
+        {
+            defaultWorld = World.Default;
+
+            InitializeShared();
+            InitializeFeatures();
+        }
+
+        protected sealed override void OnEnable() => EnableFeatures();
+
+        protected sealed override void OnDisable() => DisableFeatures();
+
+        protected void OnDestroy()
+        {
+            defaultWorld.Dispose();
+        }
+
+        // Here some services init
+        protected abstract void InitializeShared();
+
+        // Here some features init
+        protected abstract UpdateFeature[] InitializeUpdateFeatures();
+
+        protected abstract FixedUpdateFeature[] InitializeFixedUpdateFeatures();
+
+        protected abstract LateUpdateFeature[] InitializeLateUpdateFeatures();
+
+        private void InitializeFeatures()
+        {
+            _updateFeatures = InitializeUpdateFeatures();
+            _fixedUpdateFeatures = InitializeFixedUpdateFeatures();
+            _lateUpdateFeatures = InitializeLateUpdateFeatures();
+        }
+
+        private void EnableFeatures()
+        {
+            int order = 0;
+
+            for (int i = 0; i < _updateFeatures.Length; i++, order++)
+                defaultWorld.AddFeature(order, _updateFeatures[i]);
+            for (int i = 0; i < _fixedUpdateFeatures.Length; i++, order++)
+                defaultWorld.AddFeature(order, _fixedUpdateFeatures[i]);
+            for (int i = 0; i < _lateUpdateFeatures.Length; i++, order++)
+                defaultWorld.AddFeature(order, _lateUpdateFeatures[i]);
+        }
+
+        private void DisableFeatures()
+        {
+            for (int i = 0; i < _updateFeatures.Length; i++)
+                defaultWorld.RemoveFeature(_updateFeatures[i]);
+            for (int i = 0; i < _fixedUpdateFeatures.Length; i++)
+                defaultWorld.RemoveFeature(_fixedUpdateFeatures[i]);
+            for (int i = 0; i < _lateUpdateFeatures.Length; i++)
+                defaultWorld.RemoveFeature(_lateUpdateFeatures[i]);
+        }
+    }
+}
