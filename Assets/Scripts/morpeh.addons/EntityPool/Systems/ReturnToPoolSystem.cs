3afda8b085235563ab62be5d0c5e2e812a9ca06b
Mexican_Man <sombrero13nn@gmail.com>
2023-08-18 01:00:24 +0300

    Added codebased base systems

--- Assets/Scripts/morpeh.addons/EntityPool/Systems/ReturnToPoolSystem.cs
+++ Assets/Scripts/morpeh.addons/EntityPool/Systems/ReturnToPoolSystem.cs
@@ -1,11 +1,10 @@
 ﻿using Scellecs.Morpeh.Addons.EntityPool.Tags;
+using Scellecs.Morpeh.Addons.Systems;
 
 namespace Scellecs.Morpeh.Addons.EntityPool.Systems
 {
-    internal sealed class ReturnToPoolSystem : ICleanupSystem
+    internal sealed class ReturnToPoolSystem : CleanupSystem
     {
-        public World World { get; set; }
-
         private readonly EntityPoolRegistry _entityPool;
 
         private Filter _inUseEntities;
@@ -15,22 +14,18 @@ namespace Scellecs.Morpeh.Addons.EntityPool.Systems
             _entityPool = entityPool;
         }
 
-        public void OnAwake()
+        public override void OnAwake()
         {
             _inUseEntities = World.Filter.With<PooledEntityTag>().Without<FreeTag>().Build();
         }
 
-        public void OnUpdate(float deltaTime)
+        public override void OnUpdate(float deltaTime)
         {
             foreach(var inUseEntity in _inUseEntities)
             {
                 if (inUseEntity.currentArchetypeLength == 1)
                     _entityPool.PoolEntity(inUseEntity);
             }
-        }
-
-        public void Dispose()
-        {
         }
     }
 }
---
+++ Assets/Scripts/morpeh.addons/Systems/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using Unity.IL2CPP.CompilerServices;
+
+namespace Scellecs.Morpeh.Addons.Systems
+{
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    public abstract class Initializer : IInitializer 
+    {
+        public World World { get; set; }
+
+        public abstract void OnAwake();
+
+        public virtual void Dispose() { }
+    }
+}
